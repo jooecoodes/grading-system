3b826418d18f2d8a76b0c3c22234e32b930af9f0
jooecoodes <jefeljoevillacorta@gmail.com>
2023-12-08 00:31:10 -0800

    Dec. 8, Html

--- main/index.php
+++ main/index.php
@@ -6,6 +6,14 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <p>Input Students Here</p>
+    <form id="studentInsertionForm">
+        <label for="studentFname">Student's First Name</label>
+        <input type="text" name="student-name" id="studentFname" placeholder="Enter First Name">
+        <label for="studentLname">Student's Last Name</label>
+        <input type="text" name="student-name" id="studentLname" placeholder="Enter First Name">
+        <label for="studentName">Student's First Name</label>
+        <input type="text" name="student-name" id="studentName" placeholder="Enter First Name">
+    </form>
 </body>
 </html>
