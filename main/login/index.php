d0489131676142cf5ed3b964a2adf74b14a98d63
Apdian <apdianloejay@gmail.com>
2024-01-12 07:49:05 +0800

    design login register

--- main/login/index.php
+++ main/login/index.php
@@ -9,17 +9,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="art.css">
     <script src="login.js"></script>
     <title>Document</title>
 </head>
 <body>
-    <form id="loginForm">
-        <label for="emailField">Email</label>
+    
+<div class="ad"> <form id="loginForm" class="form">
+       <p class="form-title">Sign in to your account</p>
+        <div class="input-container">
+           <label for="emailField">Email</label>
         <input type="text" name="email" id="emailField" placeholder="Enter Email">
-        <label for="passwordField">Password</label>
+          <span>
+          </span>
+      </div>
+      <div class="input-container">
+      <label for="passwordField">Password</label>
         <input type="password" name="password" id="passwordField" placeholder="Enter Password">
-        <input type="submit" name="submit-btn" value="Submit" id="submitBttn">
+        </div>
+        <a href="../register/index.php">not register ?</a>
+         <button type="submit" class="submit" value="submit" id="submitBttn">
+        Sign in
+      </button>
     </form>
+   
+
+    
+   </form></div>
+
+   
+
 </body>
 </html>
         <?php
